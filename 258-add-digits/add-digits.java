db23bff410ec00a5f8c1f59a3d7115da8f1d16f5
Sahil Chaudhary <sahilchaudharygkc@gmail.com>
2026-08-06 13:40:49 +0530

    Time: 1 ms (98.05%) | Memory: 42.3 MB (91.67%) - LeetSync

---
+++ 258-add-digits/add-digits.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int addDigits(int num) {
+        int sum=num;
+        while(sum /10 !=0){
+            sum = (sum %10 ) + (sum /10);
+        }
+        return sum;
+    }
+}
