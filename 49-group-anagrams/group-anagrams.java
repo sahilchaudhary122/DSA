a4ba4a099f6e743447e7afad0e9b3da216291734
Sahil Chaudhary <sahilchaudharygkc@gmail.com>
2026-09-06 21:50:08 +0530

    Time: 7 ms (72.37%) | Memory: 49.7 MB (59.50%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.java
@@ -0,0 +1,24 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        if (strs == null || strs.length == 0) {
+            return new ArrayList<>();
+        }
+        
+        Map<String, List<String>> map = new HashMap<>();
+        
+        for (String s : strs) {
+            // Convert string to character array, sort it, and convert back to string
+            char[] charArray = s.toCharArray();
+            Arrays.sort(charArray);
+            String sortedKey = new String(charArray);
+            
+            // If the key doesn't exist, initialize a new list
+            map.putIfAbsent(sortedKey, new ArrayList<>());
+            
+            // Add the original string to its corresponding anagram group
+            map.get(sortedKey).add(s);
+        }
+        
+        return new ArrayList<>(map.values());
+    }
+}
