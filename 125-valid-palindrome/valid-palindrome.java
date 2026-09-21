2e5f49df7f334050e9248b66f7f97346be750e14
Sahil Chaudhary <sahilchaudharygkc@gmail.com>
2026-09-21 13:54:08 +0530

    Time: 2 ms (99.30%) | Memory: 44.3 MB (73.04%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,26 +1,44 @@
 class Solution {
     public boolean isPalindrome(String s) {
         int left=0,right=s.length()-1;
-        while(left <= right){
+        // while(left <= right){
+        //     char lch=s.charAt(left);
+        //     if(!Character.isLetterOrDigit(lch)){
+        //         left++;
+        //         continue;
+        //     }
+
+        //     char rch=s.charAt(right);
+        //     if(!Character.isLetterOrDigit(rch)){
+        //         right--;
+        //         continue;
+        //     }
+
+        //     if(Character.toLowerCase(lch) != Character.toLowerCase(rch)){
+        //         return false;
+                
+        //     }
+        //     left++;
+        //     right--;
+
+        // }
+
+        while(left < right){
             char lch=s.charAt(left);
-            if(!Character.isLetterOrDigit(lch)){
+            char rch=s.charAt(right);
+            if(!((lch>='a' && lch<='z') || (lch>='A' && lch<='Z') || (lch>='0' && lch<='9'))){
                 left++;
                 continue;
             }
-
-            char rch=s.charAt(right);
-            if(!Character.isLetterOrDigit(rch)){
+            if(!((rch>='a' && rch<='z') || (rch>='A' && rch<='Z') || (rch>='0' && rch<='9'))){
                 right--;
                 continue;
             }
-
-            if(Character.toLowerCase(lch) != Character.toLowerCase(rch)){
+            if( Character.toLowerCase(lch) != Character.toLowerCase(rch)){
                 return false;
-                
             }
             left++;
             right--;
-
         }
         return true;
 
